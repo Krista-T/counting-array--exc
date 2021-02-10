9a3c3b75d115f730a8b5f017581eb4838acafc87
krista <krista.tomicic@gmail.com>
2021-02-10 12:21:47 +0100

    final commit

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 "use strict";
-const numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 34, 89, 900, 2];
 let countArr = [];
 let i = 0;
 function countMe() {
   for (i = 0; i < 9; i++) {
-    // console.log(numberArr[i]);
-    // console.log(countArr);
+    task(i);
   }
-  setInterval(() => {
-    console.log(countArr.push(numberArr[i]));
-  }, 1000);
+
+  function task(i) {
+    setTimeout(function () {
+      countArr.unshift(numberArr[i]);
+      console.log(countArr);
+    }, i * 1000);
+  }
 }
+
 countMe();
